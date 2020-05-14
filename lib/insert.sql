367e0b691f8d8b27aff5bb514b1dbd1ab5f0e462
Devone Austin <devoneaustin@Philips-MacBook-Pro.local>
2020-05-14 11:54:44 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 3, "M", "dark brown", "apathetic", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 6, "F", "tan", "jubilant", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 1, "F", "black", "lazy", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 5, "F", "dark brown", "irritable", 1);
+INSERT INTO bears VALUES (5, "Melissa", 2, "F", "tan", "calm", 1);
+INSERT INTO bears VALUES (6, "Grinch", 13, "M", "black", "anxious", 1);
+INSERT INTO bears VALUES (7, "Wendy", 1, "F", "tan", "reserved", 1);
+INSERT INTO bears VALUES (8, null, 2, "M", "brown", "curious", 1);
